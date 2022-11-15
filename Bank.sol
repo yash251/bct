3f68b4fa11dc87c13b25ea7b7585bc2e6a5bd260
Yash Jagtap <jagtapyash2512@gmail.com>
2022-11-15 10:39:18 +0530

    added comments

--- Bank.sol
+++ Bank.sol
@@ -3,17 +3,19 @@ pragma solidity ^0.8.7;
 
 contract BankAccount {
     address public owner;
-    uint256 public balance;
+    uint256 public balance; // public keyword creates a getter function for this variable. 3. returns balance
 
     constructor() {
-        owner = msg.sender;
+        owner = msg.sender; // sets owner to the address of the person who deployed the contract
     }
 
     receive() external payable {
+        // 1. deposit money function
         balance += msg.value;
     }
 
     function withdraw(uint256 amount, address payable destAddress) public {
+        // 2. withdraw money function
         require(msg.sender == owner, "Only owner can withdraw");
         require(amount <= balance, "Insufficent funds");
 
--- StudentData.sol
+++ StudentData.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.7;
 
 contract StudentData {
-    event Log(string func, address sender, uint256 value, bytes data);
+    event Log(string func, address sender, uint256 value, bytes data); // used to log events
     struct Student {
+        // struct is a custom data type
         uint256 id;
         uint256 marks;
         string name;
     }
-    Student[] students;
+    Student[] students; // array of students
     uint256 nextId = 0;
 
     function Create(uint256 marks, string memory name) public {
@@ -50,7 +51,7 @@ contract StudentData {
     }
 
     fallback() external payable {
-        emit Log("fallback", msg.sender, msg.value, msg.data);
+        emit Log("fallback", msg.sender, msg.value, msg.data); // fires an event
     }
 
     receive() external payable {}
